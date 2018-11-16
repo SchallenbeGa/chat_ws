d91966ab8cd44d0efa7bc1d614ce4ba34180ae1e
Gabriel <gabriel.schallenberger@rpn.ch>
2018-11-16 10:36:57 +0100

    ajout de test et correction des routes pour la création/suppr de salon

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var bodyParser = require('./node_modules/body-parser')
 //Routes functions
 var user_log = require('./routes/r_user_log')
 var user_list = require('./routes/r_user')
+var salon = require('./routes/r_salon')
 
 const checkAuth = require('./modules/check_auth')
 var app = express()
@@ -14,5 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', user_log)
 //necessite token
 app.use('/user', user_list)
+
+app.use('/salon', salon)
 
 module.exports = app;
