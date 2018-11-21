8af3312d199b286c564efc100b612d04c2ffd981
SchallenbeGa <gabriel.schallenberger@rpn.ch>
2018-11-21 17:42:45 +0100

    Ajout de la route

--- routes/r_user.js
+++ routes/r_user.js
@@ -3,4 +3,10 @@
 var express = require('express');
 var router = express.Router();
 
+var feature = require('../modules/features_user')
+
+router.get("/:userID/friends", (req, res, next) => {
+ res = feature.getUserFriends(req,res)
+});
+
 module.exports = router;
