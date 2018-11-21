15d47e9238d7eb0f1d96f275e70a87797d373a60
SchallenbeGa <gabriel.schallenberger@rpn.ch>
2018-11-21 17:34:20 +0100

    Ajout du fichier de fonctions et de la route

--- routes/r_user.js
+++ routes/r_user.js
@@ -3,4 +3,10 @@
 var express = require('express');
 var router = express.Router();
 
+var feature = require('../modules/features_user')
+
+router.get("/:userID/salons", (req, res, next) => {
+ res = feature.getUserSalons(req,res)
+});
+
 module.exports = router;
