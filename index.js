70eca49cbfdfcfb4986b07866b18a266b3f31ff7
Schallenberger Gabriel <SchallenbeGa@s2.rpn.ch>
2018-11-22 11:46:23 +0100

    Création du serveur de socket
    WIP ajout de l'utilisateur dans un salon

--- index.js
+++ index.js
@@ -2,20 +2,35 @@ var db = require('./config/db')
 var express = require('express')
 var app = express();
 var port = process.env.PORT || 3000
+var server = require('http').createServer(app);
+var io = require("socket.io")(server);
+var r_user = require('./routes/r_user')
+
 app.set('port', port);
 
-var r_user = require('./routes/r_user')
+
+
+io.on('connection', function(socket){  
+  socket.on('join', function (room,userID) {
+    if(room!=null&&userID!=null){
+    socket.join(room);
+    console.log(room,userID)
+    }
+  });        
+});
+
 
 db.connect(function(err) {
     if (err) {
       console.log('Unable to connect to MySQL.')
       process.exit(1)
     } else {
-      app.listen(port, function() {
+      server.listen(port, function() {
         console.log('Listening on port 3000...')
       })
     }
   })
+
 
 app.use('/user',r_user)
 
