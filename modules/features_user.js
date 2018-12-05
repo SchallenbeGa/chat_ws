55372b78d5a6f282bfe51b311f6bfa33810897f7
Schallenberger Gabriel <SchallenbeGa@s2.rpn.ch>
2018-12-05 12:11:57 +0100

    Correction de la fonction et ajout des réponses JSON

--- modules/features_user.js
+++ modules/features_user.js
@@ -3,39 +3,40 @@ var mysql = require('mysql')
 
 
 function checkIfExist(userName){
-
-  var format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-  if(userName.length>125||format.test(userName)){
-    console.log('wrong username')
-  }
   var sql = 'SELECT tbl_users.userID FROM tbl_users WHERE tbl_users.userName = ' + mysql.escape(userName);
   db.query(sql, function (err, result) {
-    if (err) {console.log(err);return false}
+    if (err) {console.log(err);return false}else{
     if(result==""){
-      console.log('shiwwt');
-      return false;
+      return false
       }else{
-        return true;
+        return true
       }
-    })
+    }
+  })
 }
 
 exports.postLogin = function(req,res) {
-  if(!checkIfExist(req.body.userName)){
-    return res.status(400).json({error:true, message: "no user with that name" });
- }else{
-  var sql = 'SELECT tbl_users.userPass WHERE tbl_users.userName LIKE ' + mysql.escape(req.body.userName);
+  if(req.body.userName==""||req.body.userPass==""){
+    return res.status(500).json({error:true, message: "no input" });
+  }
+  if(Boolean(checkIfExist(req.body.userName))){
+    return res.status(200).json({error:true, message: "no user with that name" });
+  }else{
+  var sql = 'SELECT tbl_users.userPass FROM tbl_users WHERE tbl_users.userName LIKE ' + mysql.escape(req.body.userName);
   db.query(sql, function (err, result) {
     if (err) {return res.status(500).json({error:true, message: "Something went wrong" });}
     if(result!=""){
-      console.log(result)
-      return res.status(200).json({ error:false,nbSalon:result.length,salons: result });
+      if(result[0].userPass==req.body.userPass){
+        return res.status(200).json({ error:false,message: "connection accepted" });
       }else{
-        return res.status(200).json({error:true, message: "user got no salon" });
+        return res.status(200).json({ error:true,message: "wrong password" });
+      }
+         }else{
+        return res.status(500).json({error:true, message: "user not found" });
       }
     })
-    }
-    }
+  }
+}
 
 exports.getUserSalons = function(req,res) {
   var sql = 'SELECT tbl_salons.salonName FROM tbl_salons INNER JOIN tbl_user_salon ON tbl_salons.salonID = tbl_user_salon.salonID INNER JOIN tbl_users ON tbl_user_salon.userID = tbl_users.userID WHERE tbl_users.userID LIKE ' + mysql.escape(req.params.userID);
